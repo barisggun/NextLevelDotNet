31d4f288533d65fdf3bcd69d86c622849d89c3c5
Barış Gün <108459412+bgun7@users.noreply.github.com>
2023-09-07 17:18:51 +0300

    Taghelpers kullanımı ve request form

---
+++ 05_ModelBlinding/Controllers/StudentController.cs
@@ -0,0 +1,46 @@
+﻿using _05_ModelBlinding.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _05_ModelBlinding.Controllers
+{
+
+    //viewmodel 
+    // dto => data transfer object
+    public class StudentController : Controller
+    {
+        public IActionResult List()
+        {
+            List<Student> students = new List<Student>();
+            students.Add(new Student() { Name = "Werner", Surname = "Heisenberg" });
+            students.Add(new Student() { Name = "Baris", Surname = "Gun" });
+            students.Add(new Student() { Name = "Oppen", Surname = "Heimer" });
+
+            return View(students);
+        }
+
+        public IActionResult Create()
+        {
+            
+            return View();
+        }
+
+        //[HttpPost]
+        //public IActionResult Create(Student student, int TCKN)
+        //{
+        //    return View();
+        //}
+
+        [HttpPost]
+        [ActionName("Create")]
+        public IActionResult CreatePost()
+        {
+            Student student = new Student();
+            student.Name = Request.Form["Name"];
+            student.Surname = Request.Form["Surname"];
+
+            int tckn = int.Parse(Request.Form["TCKN"]);
+
+            return View();
+        }
+    }
+}
---
+++ 05_ModelBlinding/Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace _05_ModelBlinding.Models
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string? TCKN { get; set; }
+    }
+}
