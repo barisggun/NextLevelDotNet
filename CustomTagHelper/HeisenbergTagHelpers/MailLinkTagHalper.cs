90947cdcd628292b5db80357ecf14c5947264cc8
Barış Gün <108459412+bgun7@users.noreply.github.com>
2023-09-08 14:53:33 +0300

    Custom tag helper oluşturma

---
+++ CustomTagHelper/HeisenbergTagHelpers/MailLinkTagHalper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace CustomTagHelper.HeisenbergTagHelpers
+{
+    public class MailLinkTagHelper : TagHelper
+    {//override metodun çalışması için nesne üretilecek o entitiyleri de içine ekleyecek
+        public string Address { get; set; }
+        public string Text { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            //context.AllAttributes["address"]
+            output.TagName = "a";
+            output.Attributes.Add("href", $"mailto: {Address}");
+            output.Content.Append(Text);
+            output.TagMode = TagMode.StartTagAndEndTag;
+        }
+    }
+}
