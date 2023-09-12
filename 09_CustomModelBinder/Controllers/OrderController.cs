b3427e6c12669accd22ffea8ee5cea38b54a9b57
Barış Gün <108459412+bgun7@users.noreply.github.com>
2023-09-12 16:12:07 +0300

    Api Query ile custom model blinding yapımı ve kullanımı

---
+++ 09_CustomModelBinder/Controllers/OrderController.cs
@@ -0,0 +1,26 @@
+﻿using _09_CustomModelBinder.CustomBinder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _09_CustomModelBinder.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        // api/order/?ay=10&gun=12&yil=2015
+        public IActionResult GetReport([ModelBinder(typeof(HeisenbergDateTimeBinder))]DateTime date)
+        {
+            //date.GetType().GetProperty("Year").GetType(); //Object sınıfının içerisinde yazılan bir metottur.
+
+            //typeof(DateTime).GetProperties();
+
+            if(date == default(DateTime))
+            {
+                return BadRequest();
+            }
+
+            return Ok(date);
+        }
+    }
+}
---
+++ 09_CustomModelBinder/CustomBinder/HeisenbergDateTimeBinder.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.WebUtilities;
+using System.Text;
+
+namespace _09_CustomModelBinder.CustomBinder
+{
+    public class HeisenbergDateTimeBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+          //IQueryCollection queryStrings =   bindingContext.HttpContext.Request.Query;
+
+          //  bool result = int.TryParse(queryStrings["year"], out int year);
+          //  if(!result)
+          //  {
+          //      bindingContext.Result = ModelBindingResult.Failed();
+          //      return Task.CompletedTask;
+          //  }
+
+          //  result = int.TryParse(queryStrings["month"], out int month);
+          //  if (!result)
+          //  {
+          //      bindingContext.Result = ModelBindingResult.Failed();
+          //      return Task.CompletedTask;
+          //  }
+
+          //  result = int.TryParse(queryStrings["day"], out int day);
+          //  if (!result)
+          //  {
+          //      bindingContext.Result = ModelBindingResult.Failed();
+          //      return Task.CompletedTask;
+          //  }
+
+          //  DateTime date = new DateTime(year, month, day);
+
+          //  bindingContext.Result = ModelBindingResult.Success(date);
+          //  return Task.CompletedTask;
+
+            int year = int.Parse(bindingContext.HttpContext.Request.Query["year"]);
+            int month = int.Parse(bindingContext.HttpContext.Request.Query["month"]);
+
+            HttpRequest request = bindingContext.HttpContext.Request;
+            request.EnableBuffering();
+            byte[] buffer = new byte[(int)request.ContentLength];
+            request.Body.ReadAsync(buffer,0,buffer.Length);
+            string dayStr = Encoding.UTF8.GetString(buffer);
+            int day = int.Parse(dayStr);
+
+            DateTime dateTime = new DateTime(year, month, day);
+            bindingContext.Result = ModelBindingResult.Success(dateTime);
+
+            return Task.CompletedTask;
+        }
+    }
+}
